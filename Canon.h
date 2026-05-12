265c736c3a2f943e829e6068a08f7ac176dbe066
Melvin <augustine.melv@gmail.com>
2026-05-12 11:25:06 +0200

    Update action_canon enum and struct Canon

--- Canon.h
+++ Canon.h
@@ -3,14 +3,14 @@
 
 #include"Affichage.h"
 
-enum action_canon {Gauche_canon, Droite_canon, Tir};
+enum action_canon {Rien, Gauche_canon, Droite_canon, Tir};
 
 typedef struct{
   int largeur, hauteur;   /* Taille du rectangle de la base du canon */
   int pos_min, pos_max;   /* Position min (à gauche) et max (à droite) */
-  int xpos, ypos;         /* Case supérieure gauche de la position du Canon */ 
+  int xpos, ypos;         /* Case supérieure gauche de la position du Canon */
   int pas;                /* Nombre de cases de déplacement d'un Canon à chaque appui */
-  int ypos_old;           /* Position précédente du Canon */ 
+  int ypos_old;           /* Position précédente du Canon */
 } Canon;
 
 Canon* Canon_initialiser(int pos_min, int pos_max, int hauteur_aff);
@@ -20,5 +20,3 @@ void Canon_afficher(Canon *C, Affichage* A);
 
 
 #endif
-
-
