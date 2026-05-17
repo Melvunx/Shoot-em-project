4c38832a711ee5ef4e7d9e9181674fa3772eb2e0
Melvin <augustine.melv@gmail.com>
2026-05-17 13:49:29 +0200

    Refactor Liste_Canard structure and initialization function

--- Canard.h
+++ Canard.h
@@ -16,17 +16,17 @@ typedef struct Mail_Canard {
 } Canard;
 
 typedef struct {
-    Canard *tete;             // tête de la liste chaînée
-    int largeur, hauteur;     // hauteur et largeur du canard (taille)
-    char apparence[5];
-    int coord_apparition ,coord_disparition;    // coordonnée d'apparition et de disparition
-    int pas;                  // pas de déplacement
-    int nb_canards, nb_max_canards;           // nombre de canards dans la liste
+    Canard *tete;
+    int largeur, hauteur;
+    int largeur_aff, hauteur_aff;   // ← dimensions réelles de l'écran
+    int coord_apparition, coord_disparition;
+    int pas;
+    int nb_canards, nb_max_canards;
 } Liste_Canard;
 
 Canard* initialiser_canard(int largeur_aff, int hauteur_aff);
 
-Liste_Canard* Liste_Canard_initialiser_vide(int largeur_aff, int hauteur_aff,int nivdiff, char * nomfic);
+Liste_Canard* Liste_Canard_initialiser_vide(int largeur_aff, int hauteur_aff,int nivdiff);
 
 void Canard_desallouer(Liste_Canard** Lcanard);
 
