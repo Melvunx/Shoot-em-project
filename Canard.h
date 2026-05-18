9e56a247e5a504677e0b4babc61909ddc70707fb
Melvin <augustine.melv@gmail.com>
2026-05-18 14:05:48 +0200

    Update Canard.h

--- Canard.h
+++ Canard.h
@@ -3,37 +3,35 @@
 
 #include "Affichage.h"
 
-enum Direction {
-  DROITE,
-  GAUCHE
-};
+enum Direction { DROITE, GAUCHE };
 
 typedef struct Mail_Canard {
-    int x;                       // coordonnée horizontale du coin haut-gauche
-    int y;                       // coordonnée verctical
-    enum Direction direction;    // direction du canard
-    struct Mail_Canard *suivant; // maillon suivant
+    int x;
+    int y;
+    int pv;
+    enum Direction direction;
+    struct Mail_Canard *suivant;
 } Canard;
 
+// Dans Liste_Canard (Canard.h)
 typedef struct {
     Canard *tete;
     int largeur, hauteur;
-    int largeur_aff, hauteur_aff;   // ← dimensions réelles de l'écran
+    int largeur_aff, hauteur_aff;
     int coord_apparition, coord_disparition;
     int pas;
-    int nb_canards, nb_max_canards;
+    int nb_canards; // nombre de canards dans l'affichage
+    int nb_max_canards; // nombre de canards max selon le niveau de difficulté
+    int nb_tues;        // ← nombre de canards tués
+    int nb_total;       // ← nombre total à tuer pour gagner
 } Liste_Canard;
 
 Canard* initialiser_canard(int largeur_aff, int hauteur_aff);
-
 Liste_Canard* Liste_Canard_initialiser_vide(int largeur_aff, int hauteur_aff,int nivdiff);
-
 void Canard_desallouer(Liste_Canard** Lcanard);
-
 void ajouter_canard(Liste_Canard* Lcanard);
-
 void Canard_afficher(Liste_Canard *Lcanard, Affichage *A);
-
 void Canard_action(Liste_Canard *Lcanard);
+
 
 #endif
