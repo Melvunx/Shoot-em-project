3d3512e719f350ee4264f80f175eb388618ea09d
Melvin <augustine.melv@gmail.com>
2026-05-18 14:08:22 +0200

    Update Flechette.h

--- Flechette.h
+++ Flechette.h
@@ -1,42 +1,30 @@
 #ifndef FLECHETTE_H
 #define FLECHETTE_H
+
 #include "Affichage.h"
 #include "Canard.h"
 
-// ===== Struct d'une fléchette (maillon) =====
 typedef struct Flechette {
-    int x;                  // coordonnée verticale (ligne)
-    int y;                  // coordonnée horizontale (colonne)
+    int x;
+    int y;
     struct Flechette* suivant;
 } Flechette;
 
-// ===== Struct liste de fléchettes =====
 typedef struct {
-    // liste chaînée
     Flechette* tete;
-
-    // informations sur l'affichage
-    int largeur;
-    int hauteur;
-    char apparence[5];
-
-    // limites verticales
-    int y_depart;           // position de départ (bas écran)
-    int y_max;              // limite haute
-
-    // gestion quantité
+    int y_depart;
+    int y_max;
     int nb_flechettes;
     int nb_max;
-
+    char apparence[5];
 } Liste_Flechette;
 
 Liste_Flechette* Liste_Flechette_initialiser_vide(int hauteur_aff, int nivdiff);
-void Flechette_desallouer(Liste_Flechette** Lflech);
-void ajouter_flechette(Liste_Flechette* Lflech, int y);
-void Flechette_action(Liste_Flechette* Lflech);
-void Flechette_afficher(Liste_Flechette* Lflech, Affichage* A);
-void Interaction_Flechette_Canard(Liste_Flechette *Lflech, Liste_Canard *Lcanard);
+void Flechette_action(Liste_Flechette* LF);
+void Flechette_afficher(Liste_Flechette* LF, Affichage* A);
+void ajouter_flechette(Liste_Flechette* LF, int y);
+void Flechette_desallouer(Liste_Flechette** LF);
 
-
+void Interaction_Flechette_Canard(Liste_Flechette *LF, Liste_Canard *LC);
 
 #endif
